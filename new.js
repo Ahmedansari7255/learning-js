17d3d06a399af39f20464b043abd7df1da1ba13d
Ahmedansari7255 <82463481+Ahmedansari7255@users.noreply.github.com>
2021-07-25 10:42:31 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,29 @@
+
+//1.
+function add(a,b){
+    var sum=a+b;
+    return sum;
+}
+
+var add1=add(5,6)
+console.log(add1)
+
+
+//2.
+var add1=add(5,6)
+console.log(add1);
+function add(a,b){
+    var sum=a+b;
+    return sum;
+}
+
+
+//3.
+
+var add2 = function(a,b){
+    var sum=a+b;
+    return sum;
+}
+
+var res = add2(6,7);
+console.log(res);
